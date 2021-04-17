93e6798859c9bf0eb457b4159c2c339b08deb057
Tyler <tylerkershner@gmail.com>
2021-04-17 17:37:40 -0400

    add cast/credits to person/movie serializers

--- apps/movie/serializers.py
+++ apps/movie/serializers.py
@@ -1,8 +1,11 @@
+from movie_app.apps.movie_credit.serializers import MovieCreditSerializer
 from movie_app.apps.movie.models import Movie
 from rest_framework import serializers
 
 
 class MovieSerializer(serializers.HyperlinkedModelSerializer):
+    cast = MovieCreditSerializer(source='get_cast', many=True)
+
     class Meta:
         model = Movie
         exclude = ['updated_at']
--- apps/person/serializers.py
+++ apps/person/serializers.py
@@ -1,8 +1,11 @@
+from movie_app.apps.movie_credit.serializers import MovieCreditSerializer
 from movie_app.apps.person.models import Person
 from rest_framework import serializers
 
 
 class PersonSerializer(serializers.HyperlinkedModelSerializer):
+    movie_credits = MovieCreditSerializer(many=True, source='get_movie_credits')
+
     class Meta:
         model = Person
         exclude = ['updated_at']
