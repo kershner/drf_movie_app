a0ffd575ed327402f029d01c0c3d849a58168a2c
Tyler <tylerkershner@gmail.com>
2021-04-17 11:04:10 -0400

    staticfiles app fixes

--- settings.py
+++ settings.py
@@ -120,12 +120,9 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, '{}/static/assets'.format(PROJECT_NAME))
+STATIC_ROOT = os.path.join(BASE_DIR, '{}/static'.format(PROJECT_NAME))
 MEDIA_ROOT = os.path.join(BASE_DIR, '{}/media'.format(PROJECT_NAME))
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, '{}/static'.format(PROJECT_NAME)),
-]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
