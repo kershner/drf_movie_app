ebc1911a71210267de127ad836eefea2a0c2384f
Tyler <tylerkershner@gmail.com>
2021-04-17 11:45:41 -0400

    add movie list/detail view, lots of template refactoring

--- apps/movie/views.py
+++ apps/movie/views.py
@@ -2,9 +2,21 @@ from rest_framework.permissions import DjangoModelPermissionsOrAnonReadOnly
 from movie_app.apps.movie.serializers import MovieSerializer
 from movie_app.apps.movie.models import Movie
 from rest_framework import viewsets
+from django.views import generic
 
 
 class MovieViewSet(viewsets.ModelViewSet):
     queryset = Movie.objects.all()
     serializer_class = MovieSerializer
     permission_classes = [DjangoModelPermissionsOrAnonReadOnly]
+
+
+class MovieListView(generic.ListView):
+    model = Movie
+    template_name = 'movie/movie_list.html'
+    paginate_by = 40
+
+
+class MovieDetailView(generic.DetailView):
+    model = Movie
+    template_name = 'movie/movie_detail.html'
--- urls.py
+++ urls.py
@@ -1,5 +1,5 @@
 from movie_app.apps.person.views import PersonViewSet
-from movie_app.apps.movie.views import MovieViewSet
+import movie_app.apps.movie.views as movie_views
 from rest_framework.routers import DefaultRouter
 from movie_app import views as main_views
 from django.urls import include, path
@@ -7,11 +7,14 @@ from django.contrib import admin
 
 
 api_router = DefaultRouter()
-api_router.register(r'movies', MovieViewSet)
+api_router.register(r'movies', movie_views.MovieViewSet)
 api_router.register(r'people', PersonViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', main_views.home),
-    path('api/', include(api_router.urls))
+    path('', main_views.home, name='home'),
+    path('api/', include(api_router.urls)),
+    path('movies/', movie_views.MovieListView.as_view(), name='movies'),
+    path('movies/<int:pk>', movie_views.MovieDetailView.as_view(), name='movie')
 ]
+
