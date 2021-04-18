f26024f0887c8071530ca561f20eb3f56279a246
Tyler <tylerkershner@gmail.com>
2021-04-18 16:10:01 -0400

    better recent_people query for home page

--- views.py
+++ views.py
@@ -1,16 +1,15 @@
 from movie_app.apps.person.models import Person
 from movie_app.apps.movie.models import Movie
+from django.db.models import Count
 from django.shortcuts import render
 from movie_app import util
 
 
 def home(request):
-    movie_pks = Movie.objects.values_list('id', flat=True)
-    people_pks = Person.objects.values_list('id', flat=True)
-
     num_results = 20
+    movie_pks = Movie.objects.values_list('id', flat=True)
     recent_movies = Movie.objects.filter(pk__in=util.get_random_pks(pks=movie_pks, limit=len(movie_pks))).all()[:num_results]
-    recent_people = Person.objects.filter(pk__in=util.get_random_pks(pks=people_pks, limit=len(people_pks))).all()[:num_results]
+    recent_people = Person.objects.filter(tmdb_image_path__isnull=False).annotate(c=Count('moviecredit')).all().order_by('-c')[:num_results]
     ctx = {
         'recent_movies': recent_movies,
         'recent_people': recent_people
