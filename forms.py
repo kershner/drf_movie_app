8aab0c0d5f3c4f58e2ab6ee7ec133ddf8881d7f0
Tyler <tylerkershner@gmail.com>
2021-04-18 22:47:43 -0400

    lots of CSS work + refactoring

--- forms.py
+++ forms.py
@@ -3,4 +3,6 @@ from django import forms
 
 class SearchForm(forms.Form):
     q = forms.CharField(max_length=100, min_length=2, label=False,
-                        widget=forms.TextInput(attrs={'placeholder': 'Search'}))
+                        widget=forms.TextInput(attrs={
+                            'placeholder': 'Search for a movie, person, or character...'
+                        }))
--- views.py
+++ views.py
@@ -6,18 +6,29 @@ from movie_app.forms import SearchForm
 from django.db.models import Count
 from django.shortcuts import render
 from movie_app import util
+import random
 
 
 def home(request):
     num_results = 20
-    movie_pks = Movie.objects.values_list('id', flat=True)
-    recent_movies = Movie.objects.filter(pk__in=util.get_random_pks(pks=movie_pks, limit=len(movie_pks))).all()[:num_results]
-    recent_people = Person.objects.filter(tmdb_image_path__isnull=False).annotate(c=Count('moviecredit')).all().order_by('-c')[:num_results]
+    movie_pks = Movie.objects.values_list('pk', flat=True)
+    popular_movies = Movie.objects.filter(id__in=util.get_random_pks(pks=movie_pks,
+                                                                    limit=len(movie_pks))).all()[:num_results]
+    people_pks = Person.objects.values_list('pk', flat=True)
+    random_people_pks = [random.choice(people_pks) for num in range(num_results)]
+    popular_people = Person.objects.filter(tmdb_image_path__isnull=False).filter(id__in=random_people_pks).all().order_by('-popularity')
+
+    character_pks = MovieCredit.objects.values_list('pk', flat=True)
+    random_character_pks = [random.choice(character_pks) for num in range(num_results)]
+    popular_characters = MovieCredit.objects.filter(pk__in=random_character_pks).all().order_by('-person__popularity')
+
     genres = Genre.objects.all()
+
     ctx = {
         'form': SearchForm(),
-        'recent_movies': recent_movies,
-        'recent_people': recent_people,
+        'popular_movies': popular_movies,
+        'popular_people': popular_people,
+        'popular_characters': popular_characters,
         'genres': genres
     }
     return render(request, 'home/home.html', ctx)
@@ -29,8 +40,8 @@ def search_results(request):
         if form.is_valid():
             q = form.cleaned_data['q']
             movie_results = Movie.objects.filter(title__icontains=q).annotate(c=Count('moviecredit')).order_by('-c').all()
-            person_results = Person.objects.filter(name__icontains=q).annotate(c=Count('moviecredit')).order_by('-c').all()
-            character_results = MovieCredit.objects.filter(role__icontains=q).order_by('-created_at').all()
+            person_results = Person.objects.filter(name__icontains=q).order_by('-popularity').all()
+            character_results = MovieCredit.objects.filter(role__icontains=q).order_by('-person__popularity').all()
             ctx = {
                 'form': form,
                 'q': q,
